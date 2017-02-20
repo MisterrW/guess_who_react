162c182055d81f65e674aa28bef4ee0a17edde46
William Robertson <w.robertson305@gmail.com>
2017-02-20 14:36:24 +0000

    heroku procfile

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.get('/', function (req, res) {
 
 app.use(express.static('client/build'));
 
-
 const server = app.listen(3000, function () {
   const host = server.address().address;
   const port = server.address().port;
